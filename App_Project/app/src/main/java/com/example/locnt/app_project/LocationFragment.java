2260b6b0cada11144f6a37953f6b5e1493f528df
david nguyen <ngthemloc@gmail.com>
2018-10-06 08:54:50 +0700

    update login register

--- App_Project/app/src/main/java/com/example/locnt/app_project/LocationFragment.java
+++ App_Project/app/src/main/java/com/example/locnt/app_project/LocationFragment.java
@@ -8,18 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.MapView;
+//import com.google.android.gms.maps.MapsInitializer;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.model.CameraPosition;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
 
 public class LocationFragment extends Fragment{
-    GoogleMap map;
-    MapView mapView;
+//    GoogleMap map;
+//    MapView mapView;
     View mView;
 
     public LocationFragment() {
--- App_Project/app/src/main/java/com/example/locnt/app_project/RegisterActivity.java
+++ App_Project/app/src/main/java/com/example/locnt/app_project/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.locnt.app_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+
+    public void clickToSaveForRegister(View view) {
+        Toast.makeText(this,"Đăng Kí Thành Công", Toast.LENGTH_LONG).show();
+    }
+
+    public void clickToBack(View view) {
+        onBackPressed();
+        finish();
     }
 }
