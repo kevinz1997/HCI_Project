232df29879e641759738f767cb1b6e11352e2f96
david nguyen <ngthemloc@gmail.com>
2018-10-08 08:35:58 +0700

    update login

--- App_Project/app/src/main/java/com/example/locnt/app_project/RegisterActivity.java
+++ App_Project/app/src/main/java/com/example/locnt/app_project/RegisterActivity.java
@@ -6,20 +6,29 @@ import android.view.View;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private  View registerView;
+    private  View progressView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+         registerView = findViewById(R.id.register_form);
+         progressView = findViewById(R.id.register_progess);
     }
 
 
     public void clickToSaveForRegister(View view) {
         Toast.makeText(this,"Đăng Kí Thành Công", Toast.LENGTH_LONG).show();
+        showProgress(true);
+
     }
 
     public void clickToBack(View view) {
         onBackPressed();
         finish();
+    }
+    private void showProgress(boolean param){
+        progressView.setVisibility(param ? View.VISIBLE : View.GONE);
+        registerView.setVisibility(param ? View.GONE : View.VISIBLE);
     }
 }
